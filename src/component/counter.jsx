4cdd64c68b9d118f12baaaca176d982d399817b7
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-02-27 13:47:56 +0000

    Update counter.jsx

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -5,20 +5,31 @@ class Counter extends Component {
         super(props);
         this.state={
             count: 0,
-            imageUrl: "https://picsum.photos/200"
+            imageUrl: "https://picsum.photos/200",
+            tags: ['tag1','tag2','tag3']
         };
         this.formatCount=this.formatCount.bind(this);
+        this.getBadgeClass=this.getBadgeClass.bind(this);
     }
 
     render() { 
         return ( 
             <div>
-                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span className={this.getBadgeClass()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
                 <br />
                 <img src={this.state.imageUrl} alt=""/>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
             </div>
          );
+    }
+
+    getBadgeClass() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
